53f041b0502c10ba7a7bd3eb198a3d8bb024be79
amenotu <kyabut@gmail.com>
2015-10-15 10:54:05 -0700

    Added findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -14,7 +14,13 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board( { n: n } );
+
+  for(var i = 0; i < n; i++) {
+    board.togglePiece(i, i);
+  }
+
+  var solution = board.rows();
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -24,7 +30,7 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = undefined; //fixm
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
